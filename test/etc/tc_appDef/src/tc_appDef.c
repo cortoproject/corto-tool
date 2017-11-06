51965c9629e76f9416ef777a0950d98935eda617
Sander Mertens <sander.mertens8@gmail.com>
2017-11-05 22:19:07 -0800

    Initial commit (split from cortoproject/corto)

---
+++ test/etc/tc_appDef/src/tc_appDef.c
@@ -0,0 +1,11 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <include/tc_appDef.h>
+
+int tc_appDefMain(int argc, char *argv[]) {
+
+    /* Insert code that must be run when component is loaded */
+
+    return 0;
+}
+
---
+++ test/etc/tc_appLocalDef/src/tc_appLocalDef.c
@@ -0,0 +1,11 @@
+/* This is a managed file. Do not delete this comment. */
+
+#include <include/tc_appLocalDef.h>
+
+int tc_appLocalDefMain(int argc, char *argv[]) {
+
+    /* Insert code that must be run when component is loaded */
+
+    return 0;
+}
+
